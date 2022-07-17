222260605267f82a99152b3c07495bd7b96278f8
yogesh098 <50641205+yogesh098@users.noreply.github.com>
2022-07-17 10:06:37 +0545

    Second commit

--- admin/index.php
+++ admin/index.php
@@ -10,7 +10,7 @@
 <html >
 <head>
   <meta charset="UTF-8">
-  <title>SUN RISE ADMIN</title>
+  <title>XXX XXX ADMIN</title>
   
   
      
--- admin/show.php
+++ admin/show.php
@@ -194,9 +194,9 @@ tr:hover .cut { opacity: 1; }
 		<header>
 			<h1>Information of Guest</h1>
 			<address >
-				<p>SUN RISE HOTEL,</p>
-				<p>New Kalmunani Road,<br>Battialoa,<br>Sri Lanka.</p>
-				<p>(+94) 65 222 44 55</p>
+				<p>XXX XXX HOTEL,</p>
+				<p>Tirpureshwore,<br>Kathamndu,<br>Nepal.</p>
+				<p>+977-9813284718</p>
 			</address>
 			<span><img alt="" src="assets/img/sun.png"></span>
 		</header>
@@ -268,7 +268,7 @@ tr:hover .cut { opacity: 1; }
 		<aside>
 			<h1><span >Contact us</span></h1>
 			<div >
-				<p align="center">Email :- info@sunrise.com || Web :- www.sunrise.com || Phone :- +94 65 222 44 55 </p>
+				<p align="center">Email :- info@xxx.com || Web :- www.xxx.com || Phone :- +977-9813284718 </p>
 			</div>
 		</aside>
 	</body>
